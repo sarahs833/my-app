459ceae0ee8b89fea7b9f37311535da8bf64c6d4
sarah wang <sarahwang47@gmail.com>
2020-03-20 23:38:24 +0800

    Updates

---
+++ precache-manifest.46d55437c3ec068b10c69ecf223c2988.js
@@ -0,0 +1,134 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b028b1dcacebbb3bee8ce5fbf1a718c2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "32763c317cdc99240596",
+    "url": "./static/css/main.81a19678.chunk.css"
+  },
+  {
+    "revision": "64036782d24d32a0e9f5",
+    "url": "./static/js/2.0e7fccfc.chunk.js"
+  },
+  {
+    "revision": "a680856a4903381d69a83276068aa227",
+    "url": "./static/js/2.0e7fccfc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32763c317cdc99240596",
+    "url": "./static/js/main.4ceeb295.chunk.js"
+  },
+  {
+    "revision": "7304c6cd1204852fb3fb",
+    "url": "./static/js/runtime-main.1451e3c5.js"
+  },
+  {
+    "revision": "adeec7fdacbb1a115dc5afc7651c1e41",
+    "url": "./static/media/add_cocktail.adeec7fd.png"
+  },
+  {
+    "revision": "1d92d7659ed1218b7fe618a3c023674b",
+    "url": "./static/media/admin_page.1d92d765.png"
+  },
+  {
+    "revision": "104fd4801100bed9c452f55bca565d02",
+    "url": "./static/media/chatroom.104fd480.png"
+  },
+  {
+    "revision": "2721443d8aec7920dc89f02abab1572c",
+    "url": "./static/media/cocktail_show.2721443d.png"
+  },
+  {
+    "revision": "1f09ea247fcf1afc333ec8342a3cc6a7",
+    "url": "./static/media/cocktails.1f09ea24.jpg"
+  },
+  {
+    "revision": "8ab89920ceca336e1f4c7c38ccc6de6f",
+    "url": "./static/media/coktail.8ab89920.jpg"
+  },
+  {
+    "revision": "b43bc4e19d381869dbcb29da49cf6e50",
+    "url": "./static/media/curious.b43bc4e1.svg"
+  },
+  {
+    "revision": "6bdb12b66244017987dd0195d83042a7",
+    "url": "./static/media/dog.6bdb12b6.jpg"
+  },
+  {
+    "revision": "fed9f17d88a642c6e9b84b4f038ad15f",
+    "url": "./static/media/fastfood.fed9f17d.svg"
+  },
+  {
+    "revision": "3ab2ea230f1494b4d85700e1c1edcece",
+    "url": "./static/media/foodindustry.3ab2ea23.svg"
+  },
+  {
+    "revision": "33b2c749c7b35db51420f1f8daca3bdc",
+    "url": "./static/media/form.33b2c749.png"
+  },
+  {
+    "revision": "08283358ac411159a37059598d43b24c",
+    "url": "./static/media/france.08283358.svg"
+  },
+  {
+    "revision": "2338e4046ad20dad889a7afb895605b7",
+    "url": "./static/media/group.2338e404.svg"
+  },
+  {
+    "revision": "6209af66f38d781c8dfe9a08cc57002e",
+    "url": "./static/media/home.6209af66.png"
+  },
+  {
+    "revision": "1748d961b30f0acd367e8fa0b1dee631",
+    "url": "./static/media/home_cocktail.1748d961.png"
+  },
+  {
+    "revision": "0b003d1146f05f13a69e1d29388ee865",
+    "url": "./static/media/meet&greet.0b003d11.png"
+  },
+  {
+    "revision": "34451c0d6823ad2f3587b68cbec6d989",
+    "url": "./static/media/meet_greet.34451c0d.png"
+  },
+  {
+    "revision": "bbefb78525bde9156f2e7a309924b3ac",
+    "url": "./static/media/myplant.bbefb785.png"
+  },
+  {
+    "revision": "f667d05acf1d0467e4bc3a09a2e1b00b",
+    "url": "./static/media/peet_coffee.f667d05a.png"
+  },
+  {
+    "revision": "5dc20827d2b024362286c35838f57f3f",
+    "url": "./static/media/plant.5dc20827.jpg"
+  },
+  {
+    "revision": "fa9fd3366075c441c7c38bf8fdff26c0",
+    "url": "./static/media/plants.fa9fd336.jpg"
+  },
+  {
+    "revision": "5cddfe1ff0a99799be0d17dd038f4c23",
+    "url": "./static/media/puppy.5cddfe1f.jpg"
+  },
+  {
+    "revision": "ca4d60490d25f7a37b5e9b43b7ea1391",
+    "url": "./static/media/scan.ca4d6049.png"
+  },
+  {
+    "revision": "1ab4227d42470606d2ac2fe4d4b9e9ef",
+    "url": "./static/media/sitter.1ab4227d.png"
+  },
+  {
+    "revision": "dc5d1771a4830c416d5d18b46563d9c5",
+    "url": "./static/media/walls.dc5d1771.jpg"
+  },
+  {
+    "revision": "436047b64b25407396b03c6724aff853",
+    "url": "./static/media/wechat.436047b6.jpg"
+  },
+  {
+    "revision": "03b4fd9e7272306f3e6545e26be6274f",
+    "url": "./static/media/young.03b4fd9e.jpg"
+  }
+]);
