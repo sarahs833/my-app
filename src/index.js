2c33ce949aa98555a61fa688435d279dd6ec68ea
sarah wang <sarahwang47@gmail.com>
2020-04-09 13:55:45 +0800

    change the margin on the project page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, combineReducers, applyMiddleware } from 'redux';
-import reduxPromise from 'redux-promise';
-import logger from 'redux-logger';
 import { HashRouter, Route, Switch } from 'react-router-dom';
 import { createHistory as history } from 'history';
 
